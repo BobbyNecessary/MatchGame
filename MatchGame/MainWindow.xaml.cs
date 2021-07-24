5b4714559dedb35ef1307e74b3b781e37d0c2ec7
Bobby Necessary <bnecessa@uncc.edu>
2021-07-24 17:17:16 -0400

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> randomEmoji = new List<string>()
+            {
+                "😈","😈",
+                "🤠","🤠",
+                "🤡","🤡",
+                "💀","💀",
+                "👻","👻",
+                "👽","👽",
+                "👾","👾",
+                "💩","💩",
+            };
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(randomEmoji.Count);
+                string nextEmoji = randomEmoji[index];
+                textBlock.Text = nextEmoji;
+                randomEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
